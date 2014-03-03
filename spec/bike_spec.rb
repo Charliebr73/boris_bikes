6b94b978222ca6910da525d3a7baaaa2f87fbe6a
Charles De Barros <charliebr73@gmail.com>
2014-03-03 16:34:15 +0000

    Implemented 'broken?' method

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,7 +3,7 @@ require "../lib/bike.rb"
 
 # we are describing the functionality of a specific class, Bike
 describe Bike do
-	
+
 	# this is a specific feature (behaviour)
 	# that we expect to be present
 	it "should not be broken after we create it" do
@@ -12,4 +12,11 @@ describe Bike do
 		# a method "broken?" that should return false
 		expect(bike).not_to be_broken
 	end
+
+	it "should be able to break" do
+		bike = Bike.new
+		bike.break
+		expect(bike).to  be_broken
+	end
+
 end
