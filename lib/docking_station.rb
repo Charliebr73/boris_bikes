45a557bb94176f225ccf8b46d3d31fa83ff58f10
Charles De Barros <charliebr73@gmail.com>
2014-03-04 17:01:22 +0000

    Extracting common files and refactoring

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,31 +1,16 @@
+# load BikeContainer
+require_relative 'bike_container'
+
 class DockingStation
+
+	# this gives us all the methods that used the nbe in this class
+	include BikeContainer
+
 	DEFAULT_CAPACITY = 10
 
 	def initialize(options = {})
-		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-		@bikes = []
+		# self.capacity is calling the capacity=() method (note the sequals sign) defnied in BikeContainer
+		# capacity (the second argument to fetch()) is calling the capacity() method in BikeContainer
+		self.capacity = options.fetch(:capacity, capacity)
 	end
-
-	def bike_count
-		@bikes.count
-	end
-
-	def dock(bike)
-		# if the capacity is reached, raise an exception
-		raise "Station is full" if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		@bikes.delete(bike)
-	end
-
-	def full?
-		bike_count == @capacity
-	end
-
-	def available_bikes
-		@bikes.reject {|bike| bike.broken? }
-	end
-
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,23 +5,19 @@ require "../lib/bike.rb"
 describe Bike do
 	let(:bike) { Bike.new }
 
-	# this is a specific feature (behaviour)
-	# that we expect to be present
+	# this is a specific feature (behaviour) that we expect to be present
 	it "should not be broken after we create it" do
 		# bike = Bike.new # initialise a new object of Bike class
-		# expect an instance of this class to have 
-		# a method "broken?" that should return false
+		# expect an instance of this class to have a method "broken?" that should return false
 		expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do
-		bike = Bike.new
 		bike.break
 		expect(bike).to  be_broken
 	end
 
 	it "should be able to get fixed" do
-		bike = Bike.new
 		bike.break
 		bike.fix
 		expect(bike).not_to be_broken
