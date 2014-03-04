8be04b5996bd99e8440d35fc614aa0d4bdbd08d2
Charles De Barros <charliebr73@gmail.com>
2014-03-04 14:29:57 +0000

    defining 'fill_station' in docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,4 +29,13 @@ describe DockingStation do
 		expect(station).to be_full
 	end
 
+	it "should not accept a bike it's full" do
+		fill_station station
+		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError)
+	end
+
+	def fill_station(station)
+		20.times { station.dock(Bike.new) }
+	end
+
 end
